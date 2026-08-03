17ec039fea5bdff8f3ff975aeb3159fe778d4d13
Mattia M. <mattiamarras430@gmail.com>
2026-08-03 02:18:58 +0200

    Update index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router, type IRouter } from "express";
+
 import healthRouter from "./health";
 import teamsRouter from "./teams";
 import matchesRouter from "./matches";
 import standingsRouter from "./standings";
 import { settingsRouter } from "./settings";
 import databaseRouter from "./database";
+import nextFootballRouter from "./nextfb";
 
 const router: IRouter = Router();
 
@@ -12,7 +14,13 @@ router.use(healthRouter);
 router.use(teamsRouter);
 router.use(matchesRouter);
 router.use(standingsRouter);
+
 router.use("/settings", settingsRouter);
 router.use(databaseRouter);
+
+/*
+ * NextFootball public API
+ */
+router.use("/nextfb", nextFootballRouter);
 
 export default router;
